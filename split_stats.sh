d0791fe660d1222e8826a668963a7739676ffab1
Jim Walker <jim@couchbase.com>
2021-09-02 13:02:11 +0100

    Add some comments to split_stats.h so I remember how it works

--- split_stats.sh
+++ split_stats.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# first run split_stats.sh
+# e.g. for each cbcollect cd cbcollect; ./split_stats.sh; cd -
+# Next run split_stats_to_buckets (for the stat of interest)
+# e.g. for X in $(ls cbcollect*); do cd $X; ~/scripts/split_stats_to_buckets.sh stats.log.vbucket-details_ ; cd -; done
+# Now run compare_vbucket_details.py (pass each file from above step as a param)
+
 # Need brew coreutils for gcsplit
 gcsplit -f stats.log_gcsplit stats.log /-u\ \@ns_server/ {*}
 # This file not much use
